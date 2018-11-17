e5f427a31c1541985b55cf3feada2636a630197f
lichking <lichking2018@aliyun.com>
2018-11-17 18:36:48 +0800

    final测试

---
+++ src/main/java/com/imook/study/immutable/ImmutableExample.java
@@ -0,0 +1,12 @@
+package com.imook.study.immutable;
+
+/**
+ * @ProjectName: concurent
+ * @Package: com.imook.study.immutable
+ * @Description:
+ * @Author: lichking2017@aliyun.com
+ * @CreateDate: 2018/11/17 6:35 PM
+ * @Version: v1.0
+ */
+public class ImmutableExample {
+}
